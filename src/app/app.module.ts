09295fb2cd43653843e49e583d09d585f47bd8f4
BuildTools <unconfigured@null.spigotmc.org>
2021-02-08 16:20:32 -0800

    Feat: Add in basic routes to the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomesComponent } from './home/home.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -15,7 +15,7 @@ import { StarsComponent } from './stars/stars.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    HomesComponent,
     CarouselComponent,
     FooterComponent,
     NavbarComponent,
